49cbb4e3ac0b2655022450739742c4f3e14a7e9f
Juan Cruz Almazán <juancruzalmazan1994@gmail.com>
2022-06-02 14:21:13 -0300

    cleaner implementation

--- controllers/cleaner.js
+++ controllers/cleaner.js
@@ -1,5 +1,36 @@
+const { FileCleaner } = require('cron-file-cleaner');
+
+const tmpWatcher = new FileCleaner('public/', 21600000, '0 */6 * * *', {
+  recursive: true,
+});
+
 const start = async (req, res) => {
   try {
+    tmpWatcher.on('delete', (file) => {
+      console.log('DELETE');
+      console.log(file.name); // Name of the file
+      console.log(file.folder); // folder path
+      console.log(file.path); // Full path of the file
+    });
+
+    tmpWatcher.on('error', (err) => {
+      console.log('ERROR');
+      console.error(err);
+    });
+
+    tmpWatcher.on('stop', (info) => {
+      console.log('STOP');
+      console.log(info.path);
+      console.log(info.cronTime);
+    });
+
+    tmpWatcher.on('start', (info) => {
+      console.log('START');
+      console.log(info.path);
+      console.log(info.cronTime);
+    });
+
+    tmpWatcher.start();
     res.status(201).send('ok');
   } catch (e) {
     console.log(e);
@@ -9,6 +40,7 @@ const start = async (req, res) => {
 
 const stop = async (req, res) => {
   try {
+    tmpWatcher.stop();
     res.status(201).send('ok');
   } catch (e) {
     console.log(e);
@@ -18,6 +50,7 @@ const stop = async (req, res) => {
 
 const clean = async (req, res) => {
   try {
+    tmpWatcher.cleanUp();
     res.status(201).send('ok');
   } catch (e) {
     console.log(e);
